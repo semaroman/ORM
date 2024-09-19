ea5bd83fd2eabdbca405023849bc4d7256232f26
semaroman <106012219+semaroman@users.noreply.github.com>
2024-09-19 21:23:52 +0300

    Create PersonFirstKey.java

---
+++ src/main/java/model/PersonFirstKey.java
@@ -0,0 +1,22 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class PersonFirstKey implements Serializable {
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false)
+    private int age;
+}
