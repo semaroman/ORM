cba79c8221df9b0907af6f87f6689535efc786f1
semaroman <106012219+semaroman@users.noreply.github.com>
2024-09-19 21:44:30 +0300

    Delete src/main/resources directory

--- src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists public.Persons;
-create table if not exists public.Persons(
-    id serial primary key,
-    name varchar(20) not null,
-    surname varchar(20) not null,
-    age int not null check(age > 0),
-    phone_number char(16),
-    city_of_living varchar(40)
-);
